5e0875dedac6d89714c21b0f8e96172c6e151442
only9464 <115201218+only9464@users.noreply.github.com>
2024-04-24 03:51:04 +0000

    update

--- mkdocs_builder/update_and_deploy.sh
+++ mkdocs_builder/update_and_deploy.sh
@@ -4,8 +4,11 @@
 current_time=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 two_hours_ago=$(date -u -d '2 hours ago' +"%Y-%m-%dT%H:%M:%SZ")
 
+# 从环境变量读取token
+token = $1
+
 # 使用 GitHub API 获取过去两小时内的提交信息
-curl -s -H "Authorization: token ${{ secrets.TOKEN }}" "https://api.github.com/repos/HEUOpenResource/heu-icicles/commits?since=$two_hours_ago&until=$current_time" > commits.json
+curl -s -H "Authorization: token $token" "https://api.github.com/repos/HEUOpenResource/heu-icicles/commits?since=$two_hours_ago&until=$current_time" > commits.json
 
 # 判断是否有提交
 if jq -e '. | length > 0' commits.json >/dev/null; then
@@ -16,8 +19,8 @@ if jq -e '. | length > 0' commits.json >/dev/null; then
     git init
     git config --local user.name "xhd0728"
     git config --local user.email "hdxin2002@gmail.com"
-    export remote_repo="https://xhd0728:${TOKEN}@github.com/HEUOpenResource/heu-icicles.git"
-    git remote add origin "${remote_repo}"
+    export remote_repo="https://xhd0728:$token@github.com/HEUOpenResource/heu-icicles.git"
+    git remote add origin $remote_repo
     mkdocs gh-deploy --force
 else
     echo "#######################仓库两小时内无更新######################"
